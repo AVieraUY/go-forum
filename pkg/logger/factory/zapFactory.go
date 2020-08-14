46ffd952b820e1cfe990fcd6723b72190d9577a3
A. Viera <j.agustin.viera@gmail.com>
2020-08-14 14:31:55 -0300

    fix logger

--- pkg/logger/factory/zapFactory.go
+++ pkg/logger/factory/zapFactory.go
@@ -11,7 +11,7 @@ type ZapFactory struct{}
 
 // Build an instance of zap logger
 func (f *ZapFactory) Build(c *config.LoggerConfig) error {
-	if err := zap.RegisterLogger(&c); err != nil {
+	if err := zap.RegisterLogger(*c); err != nil {
 		return errors.Wrap(err, "Failed to register logger")
 	}
 	return nil
